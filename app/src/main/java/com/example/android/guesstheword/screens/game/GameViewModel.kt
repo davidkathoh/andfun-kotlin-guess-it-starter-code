854b1ce4dd852e589d1fc9551a2152531fb91a4b
davidKathoh <mudavidkat@gmail.com>
2019-05-15 09:47:51 +0200

    Step.04-Exercise-Add-LiveData-Encapsulation.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,23 +1,29 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel:ViewModel() {
 
     // The current word
-     var word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word:LiveData<String>
+            get() = _word
 
     // The current score
-     var score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score:LiveData<Int>
+            get() = _score
+
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
     init {
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
 
     }
 
@@ -58,18 +64,18 @@ class GameViewModel:ViewModel() {
         if (wordList.isEmpty()) {
            // gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
 
     }
 
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (_score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (_score.value)?.plus(1)
         nextWord()
     }
 
