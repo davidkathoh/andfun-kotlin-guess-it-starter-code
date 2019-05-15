4a38c04a17a2d0aec36bf79f78dd2878bd451c08
davidKathoh <mudavidkat@gmail.com>
2019-05-15 08:52:22 +0200

    Step.02-Exercise-Populate-the-GameViewModel.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -3,13 +3,77 @@ package com.example.android.guesstheword.screens.game
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class GameViewModel:ViewModel(){
+class GameViewModel:ViewModel() {
+
+    // The current word
+     var word = ""
+
+    // The current score
+     var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
     init {
-        Log.i("GameViewModel","View model cleared")
+        resetList()
+        nextWord()
+        Log.i("GameViewModel","View model created")
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewMode","view model cleared")
     }
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+           // gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+
+    }
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
+    }
+
+
 }
